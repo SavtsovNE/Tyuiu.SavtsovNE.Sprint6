043254d6f678d993d9bbe807ad21ffb0e902e9ae
Никита Савцов <nsavtsov89@inbox.ru>
2024-12-04 22:39:22 +0500

    Task0

--- Tyuiu.SavtsovNE.Sprint6.Task0.V15.Lib/DataService.cs
+++ Tyuiu.SavtsovNE.Sprint6.Task0.V15.Lib/DataService.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.SavtsovNE.Sprint6.Task0.V15.Lib
 {
     public class DataService : ISprint6Task0V15
     {
-        public double Calculate(int  x)
+        public double Calculate(int x)
         {
             double res = Math.Pow(x, 3) + 2 * Math.Pow(x, 2) + 5 * x + 4;
             return Math.Round(res, 3);
