de0ab19acf1006461efaa3fb82f148995a26b0b8
Никита Савцов <nsavtsov89@inbox.ru>
2024-12-05 22:16:45 +0500

    Task5

---
+++ Tyuiu.SavtsovNE.Sprint6.Task5.V9.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SavtsovNE.Sprint6.Task5.V9.Lib;
+using System.IO;
+
+namespace Tyuiu.SavtsovNE.Sprint6.Task5.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\karpo\source\repos\Tyuiu.KarpovAA.Sprint6\Tyuiu.KarpovAA.Sprint6.Task5.V9\bin\Debug\InPutFileTask5V9.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
